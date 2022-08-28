dbd672f088070aa285693a552d37e185f94aa20a
Simon Larsén <slarse@kth.se>
2022-08-28 22:45:03 +0200

    Put both underscore and minus on default layer

--- keyboards/crkbd/keymaps/slarse/keymap.c
+++ keyboards/crkbd/keymaps/slarse/keymap.c
@@ -22,13 +22,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_MINS,
+       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_UNDS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,  HOME_A,  HOME_R,  HOME_S,  HOME_T,    KC_D,                         KC_H,  HOME_N,  HOME_E,  HOME_I,  HOME_O, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,  KC_TAB,  L1_SPC,     L2_ENT,   MO(2), KC_BSPC
+                                          KC_LGUI,  KC_TAB,  L1_SPC,     L2_ENT, KC_MINS, KC_BSPC
                                       //`--------------------------'  `--------------------------'
 
   ),
