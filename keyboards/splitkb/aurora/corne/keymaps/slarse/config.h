71717a4bb77f03ef2f4d81beb520d9e7babfe50e
Simon Larsén <slarse@slar.se>
2024-11-29 23:41:51 +0100

    Add keymap to aurora

---
+++ keyboards/splitkb/aurora/corne/keymaps/slarse/config.h
@@ -0,0 +1,71 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+//#define TAPPING_FORCE_HOLD
+
+// Home row mods
+#define TAPPING_TERM 175
+#define TAPPING_FORCE_HOLD
+
+#define HOME_T LSFT_T(KC_T)
+#define HOME_S LCTL_T(KC_S)
+#define HOME_R LALT_T(KC_R)
+#define HOME_A LGUI_T(KC_A)
+
+#define HOME_N RSFT_T(KC_N)
+#define HOME_E RCTL_T(KC_E)
+#define HOME_I LALT_T(KC_I)
+#define HOME_O LGUI_T(KC_O)
+
+#define HOME_LT LGUI_T(KC_LEFT)
+#define HOME_DW LALT_T(KC_DOWN)
+#define HOME_RT LCTL_T(KC_RIGHT)
+
+// Layers
+#define L1_SPC LT(1, KC_SPC)
+#define L2_ENT LT(2, KC_ENT)
+#define L2_MINS LT(2, KC_MINS)
+
+#ifdef RGBLIGHT_ENABLE
+    #define RGBLIGHT_EFFECT_BREATHING
+    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    #define RGBLIGHT_EFFECT_SNAKE
+    #define RGBLIGHT_EFFECT_KNIGHT
+    #define RGBLIGHT_EFFECT_CHRISTMAS
+    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #define RGBLIGHT_EFFECT_RGB_TEST
+    #define RGBLIGHT_EFFECT_ALTERNATING
+    #define RGBLIGHT_EFFECT_TWINKLE
+    #define RGBLIGHT_LIMIT_VAL 120
+    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_SAT_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
+#endif
